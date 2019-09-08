f46209a208d4341a96a785c04f3f28b388ac77ab
jannatul Fardus Retu <jannatulfardusretu@jannatuls-mbp.lan>
2019-09-08 15:59:24 -0400

    added project

---
+++ TestnggroupStudy/src/test/java/com/alokhao/test/Reggresion.java
@@ -0,0 +1,5 @@
+package com.alokhao.test;
+
+public class Reggresion {
+
+}
---
+++ TestnggroupStudy/src/test/java/com/alokhao/test/SmokeTest.java
@@ -0,0 +1,26 @@
+package com.alokhao.test;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest{
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");	
+	}
+	@Test
+    public void test3() {
+	
+    	System.out.println("test3");	
+	}
+
+    //what is @suit?
+	//when we use it?
+	//why we use testng.xml?
+	//when we use?
+	//in xml what we do with source and design?
+	//which annotation we have to use against this method
+}
